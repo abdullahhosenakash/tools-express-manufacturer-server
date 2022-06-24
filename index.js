fe685b1e1334759cc14a9b7e2d1c35da02b5e6a9
abdullahhosenakash <abdullahhosenakash@gmail.com>
2022-06-24 17:58:34 +0600

    jwt token created

--- index.js
+++ index.js
@@ -13,7 +13,20 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c1vp23d.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unAuthorized Access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 async function run() {
     try {
@@ -32,6 +45,12 @@ async function run() {
         app.get('/reviews', async (req, res) => {
             const query = {};
             const result = await reviewCollection.find(query).sort({ _id: -1 }).toArray();
+            res.send(result);
+        });
+
+        app.get('/orders', verifyJWT, async (req, res) => {
+            const query = {};
+            const result = await orderCollection.find(query).toArray();
             res.send(result);
         });
 
